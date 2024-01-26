439abeabbb32b8658f3db86996cdaad5fec95534
AnayochukwuJames <anayochukwujames11@gmail.com>
2024-01-27 00:56:50 +0100

    update initial commit

---
+++ src/main/java/com/example/taskmasters/model/User.java
@@ -0,0 +1,28 @@
+package com.example.taskmasters.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private String phoneNumber;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+}
