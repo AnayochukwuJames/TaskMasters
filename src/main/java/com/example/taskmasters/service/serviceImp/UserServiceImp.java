cb5ad3c4ef4b29f975ae877ac0404ffe431a4306
AnayochukwuJames <anayochukwujames11@gmail.com>
2024-02-13 12:26:12 +0100

    update initial commit

--- src/main/java/com/example/taskmasters/service/serviceImp/UserServiceImp.java
+++ src/main/java/com/example/taskmasters/service/serviceImp/UserServiceImp.java
@@ -1,12 +1,10 @@
 package com.example.taskmasters.service.serviceImp;
 
-import com.example.taskmasters.dto.EmailRequest;
-import com.example.taskmasters.dto.GoogleSignUpRequest;
-import com.example.taskmasters.dto.SignUpRequest;
-import com.example.taskmasters.dto.SignUpResponse;
+import com.example.taskmasters.dto.*;
 import com.example.taskmasters.entities.MailMessages;
 import com.example.taskmasters.entities.Users;
 import com.example.taskmasters.repository.UserRepository;
+import com.example.taskmasters.service.JWTService;
 import com.example.taskmasters.service.UserService;
 import com.example.taskmasters.validation.PasswordValidator;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +12,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,6 +27,7 @@ public class UserServiceImp implements UserService {
     private final UserRepository userRepository;
 
     private final PasswordEncoder passwordEncoder;
+    private final JWTService jwtService;
 
     @Override
     public ResponseEntity<SignUpResponse> signUp(SignUpRequest signUpRequest) {
@@ -88,5 +90,34 @@ public class UserServiceImp implements UserService {
         } catch (Exception e) {
             throw new RuntimeException("Error during Google signup", e);
         }
+    }
+    @Override
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        Users user = userRepository.findByUsername(userEmail);
+//                .orElseThrow (() -> new UsernameNotFoundException("User not found with email: " + userEmail));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+
+    }
+
+    public UserDetailsService userDetailsService() {
+        return (UserDetailsService) this;
+    }
+    @Override
+    public JwtAuthenticationResponse refreshToken(RefreshTokenRequest refreshTokenRequest){
+        String userEmail = jwtService.extractUserName(refreshTokenRequest.getToken());
+        Users users = (Users) userRepository.findByEmail(userEmail).orElseThrow();
+        if (jwtService.isTokenValid(refreshTokenRequest.getToken(), users));
+
+        var jwt = jwtService.generateToken(users);
+        JwtAuthenticationResponse jwtAuthenticationResponse = new JwtAuthenticationResponse();
+        jwtAuthenticationResponse.setToken(jwt);
+        jwtAuthenticationResponse.setRefreshToken(refreshTokenRequest.getToken());
+
+        return jwtAuthenticationResponse;
     }
 }
